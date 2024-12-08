6ca6325d260d1f5e7ef962559e4b850a3f6f6a85
starinje <starinje@gmail.com>
2024-12-08 12:12:18 -0500

    Docker Compose, Taskfile, and Migrations

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,12 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+
+	"github.com/TutorialEdge/go-rest-api-course/internal/db"
+)
 
 // Run - is going to be responsible for
 // the initialization and startup of our
 // go application
 func Run() error {
 	fmt.Println("Starting up our go application...")
+
+	db, err := db.NewDatabase()
+	if err != nil {
+		fmt.Println(err)
+		return fmt.Errorf("failed to connect to the database")
+	}
+
+	if err := db.MigrateDB(); err != nil {
+		fmt.Println("Failed to migrate database")
+		return err
+	}
+
+	if err := db.Ping(context.Background()); err != nil {
+		return err
+	}
+
+	fmt.Println("Successfully connected and pinged database!")
+
 	return nil
 
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,12 @@
 module github.com/TutorialEdge/go-rest-api-course
 
 go 1.23.3
+
+require (
+	github.com/golang-migrate/migrate/v4 v4.18.1 // indirect
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	github.com/jmoiron/sqlx v1.4.0 // indirect
+	github.com/lib/pq v1.10.9 // indirect
+	go.uber.org/atomic v1.11.0 // indirect
+)
