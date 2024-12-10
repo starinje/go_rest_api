f245d9939df15efb3e2a7e5a098a4b8d296a193a
starinje <starinje@gmail.com>
2024-12-09 21:10:58 -0500

    A few more methods

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,7 +4,9 @@ import (
 	"context"
 	"fmt"
 
-	"github.com/TutorialEdge/go-rest-api-course/internal/db"
+	"go_rest_api/internal/comment"
+
+	"go_rest_api/internal/db"
 )
 
 // Run - is going to be responsible for
@@ -23,6 +25,23 @@ func Run() error {
 		fmt.Println("Failed to migrate database")
 		return err
 	}
+
+	cmtService := comment.NewService(db)
+
+	cmtService.PostComment(
+		context.Background(),
+		comment.Comment{
+			ID:     "ff1d4530-959b-4a2d-a12f-47c095350e1b",
+			Slug:   "my-first-comment",
+			Author: "Elliott",
+			Body:   "Hello, World",
+		},
+	)
+
+	fmt.Println(cmtService.GetComment(
+		context.Background(),
+		"ff1d4530-878b-4a2d-a12f-47c095350e1b",
+	))
 
 	if err := db.Ping(context.Background()); err != nil {
 		return err
--- go.mod
+++ go.mod
@@ -1,12 +1,17 @@
-module github.com/TutorialEdge/go-rest-api-course
+module go_rest_api
 
 go 1.23.3
 
 require (
-	github.com/golang-migrate/migrate/v4 v4.18.1 // indirect
+	github.com/golang-migrate/migrate/v4 v4.18.1
+	github.com/jmoiron/sqlx v1.4.0
+	github.com/lib/pq v1.10.9
+	github.com/satori/go.uuid v1.2.0
+)
+
+require (
 	github.com/hashicorp/errwrap v1.1.0 // indirect
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
-	github.com/jmoiron/sqlx v1.4.0 // indirect
-	github.com/lib/pq v1.10.9 // indirect
 	go.uber.org/atomic v1.11.0 // indirect
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 )
